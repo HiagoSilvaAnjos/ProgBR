db0313d305f0af458b76478efc2681a28db6289e
Hiago Silva <hiagosilvaanjos7@gmail.com>
2022-01-21 08:25:30 -0300

    Update script.js

--- JavaScript_basico/arrays/javascript/script.js
+++ JavaScript_basico/arrays/javascript/script.js
@@ -2,21 +2,21 @@
 
 // existe algumas formas de escrever uma arrays, uma delas, é da seguinte maneira
 
-// var alunos = new array();
+var alunos = new Array();
 
-// console.log(alunos);
+console.log(alunos);
 
 // Èssa é uma das maneiras de escrever uma array, usando a palavra rezevada “new array()” Nesse caso não temos nada dentro da array  vamos acrescentar par ver como ele se comporta.
 
-// var alunos = new Array("Felipe", "40", "true", 40);
-// console.log(alunos[3]);
+var alunos2 = new Array("Felipe", "40", "true", 40);
+console.log(alunos2[3]); // Vai imprimir o valor 40 que é o index 3 do Array
 
-// cada elemento dentro do Array precisa ser separado com um virgula, no exemplo aqui  só tem string, mas poderia ter qualquer dado numerto string boolean, todos os tipos de dados dentro
+// cada elemento dentro do Array precisa ser separado com um virgula, no exemplo aqui  só tem string, mas poderia ter qualquer dado numérico, string, boolean, todos os tipos de dados dentro
 
 // podemos selecionar um dado especifico para aparecer do array pra isso usamos o seu index ou índice que pode ser acessado colocando dentro de alunos que está no console colchetes e entre parêntese o numero da posição.
 
-// var alunos = new Array("Felipe", "Igor", "Jose", "Marcelo");
-// console.log(alunos[0]);
+var alunos3 = new Array("Felipe", "Igor", "Jose", "Marcelo");
+console.log(alunos3[0]); // Aqui será impresso "Felipe"
 
 // outra maneira de criar um array é começar  entre colchetes colocando os dados.
 
@@ -29,8 +29,8 @@
 
 // podemos perceber que ele mostra que o array inicia no zero, só que embaixo mostra Length:4 que é o tamanho do nosso array então ainda podemos adaptar da seguinte maneira.
 
-// var alunos = ["Felipe", "Igor", "Jose", "Marcelo"];
-// console.log(alunos.length);
+var alunos4 = ["Felipe", "Igor", "Jose", "Marcelo"];
+console.log(alunos4.length); // Aqui vai imprimir 4 que é o tamanho do Array
 
 // E assim dentro do console  ele vai mostra só o tamanho do array.
 
@@ -38,11 +38,11 @@
 
 // Então podemos combinar da seguinte maneira
 
-// var alunos = ["Felipe", "Igor", "Jose", "Marcelo"];
+var alunos5 = ["Felipe", "Igor", "Jose", "Marcelo"];
 
-// for ( var i = 0; i < alunos.length; i++){
-//     console.log(alunos[i]);
-// }
+for ( var i = 0; i < alunos5.length; i++){
+    console.log(alunos5[i]); // Vai imprimir todos os elementos dentro do Array
+}
 // Então temos criamos uma variante chamada i que é igual a 0
 
 // e dizemos que se i for menor que alunos.length ( Isso porque  length representa o tamanho da array então é um numero que faz sentido nessa equação de i ser menor que length) i++ ou seja o i vai somar mais um
@@ -55,32 +55,31 @@
 
 // por exemplo
 
-// var alunos = ["Felipe", "Igor", "Jose", "Marcelo", "Paulo"];
+var alunos6 = ["Ana", "Carlos", "Rodrigo", "Felipe", "João"];
 
-// for ( var i in alunos){
-    
-//     console.log(alunos[i]);
-// }
+for ( var i in alunos6){
+    console.log(alunos6[i]);
+}
 // com o “in” a gente faz com que a gente pegue o index do array e traga pra dentro da variante, “i” assim no console podemos pedir pra que o “i” mostre todo index da variante alunos
 
 // e com o “of”
-// var alunos = ["Felipe", "Igor", "Jose", "Marcelo"];
+// var alunos6 = ["Ana", "Carlos", "Rodrigo", "Felipe", "João"];
 
-// for ( var aluno of alunos){
+// for ( var aluno of alunos6){
 
 //     console.log(aluno);
 // }
 
 // já o “of” pega todo elemento do array e tras pra dentro da variante assim a gente não precisa mais usar a variante do array entre colchete, basta a  gente escreve a variante do for que todo elemento ou seja os dados vai aparecer
 
-// var alunos = [
-// "Felipe",
-// "Igor",
-// "Jose",
-// "Marcelo",
-// "Cláudia"
-// ];
+var alunos7 = [
+"Felipe",
+"Igor",
+"Jose",
+"Marcelo",
+"Cláudia"
+];
 
-// for (var aluno of alunos) {
-//     console.log(aluno);
-// }
+for (var aluno of alunos7) {
+    console.log(aluno);
+}
