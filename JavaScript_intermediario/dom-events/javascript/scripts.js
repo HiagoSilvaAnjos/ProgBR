2c9a02a0a92e33536ee6b6fee9273e396320cd52
Hiago Silva <hiagosilvaanjos7@gmail.com>
2022-01-26 11:09:08 -0300

    Novas aulas

---
+++ JavaScript_intermediario/dom-events/javascript/scripts.js
@@ -0,0 +1,20 @@
+function sumirH1() {
+    let h = document.getElementById('h1');
+    h.style.display = 'none';
+    
+}
+
+function aparecerH1() {
+    let h1 = document.getElementById('h1');
+    h1.style.display = 'block';
+}
+
+function mudarfont() {
+    let h1 = document.getElementById('h1');
+    h1.style.fontFamily = 'Arial';
+}
+
+function voltarfont() {
+    let h1 = document.getElementById('h1');
+    h1.style.fontFamily = 'serif';
+}
