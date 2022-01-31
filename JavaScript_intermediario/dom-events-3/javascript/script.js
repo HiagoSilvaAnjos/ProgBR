84333de98da145d3b5113cf705e6be5abca7e463
Hiago Silva <hiagosilvaanjos7@gmail.com>
2022-01-31 18:44:25 -0300

    Update script.js

--- JavaScript_intermediario/dom-events-3/javascript/script.js
+++ JavaScript_intermediario/dom-events-3/javascript/script.js
@@ -4,22 +4,21 @@ let modificarElement  = document.getElementById('itemMod');
  let estado = "sim"
 mudarElement.addEventListener('click', () => {
     
-
-   
-
     if(estado === "sim") {
-        modificarElement.style.backgroundColor = "crimson"
-        modificarElement.style.color = "white"
-        modificarElement.style.padding = "10px"
-        modificarElement.style.fontFamily = "arial"
-        modificarElement.style.borderRadius = "4px"
-        estado = "não"
+        modificarElement.innerHTML = "Novo título";
+        modificarElement.style.backgroundColor = "crimson";
+        modificarElement.style.color = "white";
+        modificarElement.style.padding = "10px";
+        modificarElement.style.fontFamily = "arial";
+        modificarElement.style.borderRadius = "4px";
+        estado = "nao";
     } else {
-        modificarElement.style.backgroundColor = "blue"
-        modificarElement.style.color = "black"
-        modificarElement.style.padding = "0px"
-        modificarElement.style.fontFamily = "serif"
-        modificarElement.style.borderRadius = "0px"
+        modificarElement.innerHTML = 'Título';
+        modificarElement.style.backgroundColor = "blue";
+        modificarElement.style.color = "black";
+        modificarElement.style.padding = "0px";
+        modificarElement.style.fontFamily = "serif";
+        modificarElement.style.borderRadius = "0px";
         estado = "sim"
     }
 })
