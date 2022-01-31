f956b9b7e1c21efc5f94f5b10daf4de651ea0e5d
Hiago Silva <hiagosilvaanjos7@gmail.com>
2022-01-31 10:34:31 -0300

    Atualizado

---
+++ JavaScript_intermediario/dom-events-2/javascript/script.js
@@ -0,0 +1,21 @@
+const buttonElement = document.querySelector('#button');
+
+buttonElement.addEventListener('mouseover', () => {
+    buttonElement.style.backgroundColor = "blue"
+})
+
+
+buttonElement.addEventListener('mouseout', () => {
+    buttonElement.style.backgroundColor = "crimson"
+})
+
+
+
+
+let button = document.getElementById('button');
+
+
+button.addEventListener('click', () => {
+    
+    img.setAttribute('src', './Projeto-Android.png')
+})
