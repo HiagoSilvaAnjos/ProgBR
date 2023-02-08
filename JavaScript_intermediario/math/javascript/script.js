c404f2491199278bf0c18f67a679694fd9931a6f
Hiago Silva <hiagosilvaanjos7@gmail.com>
2023-02-08 15:05:20 -0300

    Update script.js

--- JavaScript_intermediario/math/javascript/script.js
+++ JavaScript_intermediario/math/javascript/script.js
@@ -8,7 +8,7 @@ let random = Math.floor(Math.random() * 10)
 if (getInfo > 10 || getInfo < 0) {
     alert(`Por favor! digite um número entre 0 e 10`)
 } else if (getInfo === random) {
-    alert(`Acertou! Você digtou ${getInfo} e o número que eu gerei foi ${random}`)
+    alert(`Acertou! Você digitou ${getInfo} e o número que eu gerei foi ${random}`)
 } else {
     alert(`Errou o número gerado foi ${random}`)
 }
