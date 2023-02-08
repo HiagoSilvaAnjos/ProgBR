f87e81517158ff350f6c4ba2be0ff62ed5b5f1df
Hiago Silva <hiagosilvaanjos7@gmail.com>
2023-02-08 10:38:21 -0300

    updates

--- JavaScript_intermediario/dom-events-2/javascript/script.js
+++ JavaScript_intermediario/dom-events-2/javascript/script.js
@@ -6,16 +6,27 @@ buttonElement.addEventListener('mouseover', () => {
 
 
 buttonElement.addEventListener('mouseout', () => {
-    buttonElement.style.backgroundColor = "crimson"
+    buttonElement.style.backgroundColor = "#fff"
 })
-
-
 
 
 let button = document.getElementById('button');
+let turn = 0;
+
+
+const turnValidate = () => {
+    if (turn === 0) {
+        img.setAttribute('src', './Projeto-Android.png');
+        turn = 1;
+    } else {
+        img.removeAttribute('src', './Projeto-Android.png');
+        turn = 0;
+    }
+}
 
 
 button.addEventListener('click', () => {
-    
-    img.setAttribute('src', './Projeto-Android.png')
+    turnValidate();
 })
+
+
