acd7cfd86a74d1d4b53b4200e249e657ae7f113b
Hiago Silva <hiagosilvaanjos7@gmail.com>
2023-03-02 10:41:24 -0300

    nova aula

---
+++ NodeJS/Aula04/script.js
@@ -0,0 +1,24 @@
+// FIXME: Trabalhando com arquivos no NODE JS
+// FIXME: O fs.appendFile() método anexa conteúdo especificado a um arquivo. Se o o arquivo não existe, o arquivo será criado:
+// FIXME: O fs.writeFile() método substitui o arquivo e o conteúdo especificados, se existir. Se o arquivo não existir, um novo arquivo, contendo o conteúdo especificado, será ser criado
+// FIXME: O fs.unlink apaga o arquivo
+// FIXME: O fs.rename renomeia o arquivo
+// FIXME: O fs.readFile ler o arquivo
+// Pesquisar na documentação para saber todos os parâmetros necessários para cada método acima
+
+[argOne, argTwo, ...rest] = process.argv;
+let fs = require('fs');
+
+let fileName = rest;
+fs.readFile(fileName, "UTF8", (err, data) => {
+    if (err) {throw 'Deu ruim'};
+
+    fs.writeFile(fileName + '-Upercase', data.toUpperCase, (err) => {
+        if (err) {throw "ops"};
+
+        console.log('Arquivo gerado com sucesso')
+    })
+
+})
+
+
