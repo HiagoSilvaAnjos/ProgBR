9b973a73499fe059c0273674dd4a8ff5450258cb
Hiago Silva <hiagosilvaanjos7@gmail.com>
2023-03-02 09:27:00 -0300

    novas aulas

---
+++ NodeJS/Aula02/script.js
@@ -0,0 +1,18 @@
+[argOne, argTwo, ...rest] = process.argv;
+
+let operation = '';
+const symbolOperation = rest[0];
+const valueOne = Number(rest[1]);
+const valueTwo = Number(rest[2]);
+
+const sum = (numOne, numTwo) => numOne + numTwo;
+const multiply = (numOne, numTwo) => numOne * numTwo;
+
+if (symbolOperation !== 's') operation = 'Ivalid';
+if (symbolOperation !== 'm') operation = 'Ivalid';
+
+if (symbolOperation === 's') operation = sum(valueOne, valueTwo);
+
+if (symbolOperation === 'm') operation = multiply(valueOne, valueTwo);
+
+console.log(operation);
---
+++ NodeJS/aula01/script.js
@@ -0,0 +1,4 @@
+const a = 10;
+const b = 90;
+const sum = (numberOne, numberTwo) => numberOne + numberTwo;
+console.log(sum(a, b));
